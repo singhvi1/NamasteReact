e4a4a5eb83c15bb86eab429a4fdbca0dc9dd693b
singhvi1 <singvi182003@gmail.com>
2025-08-15 00:21:08 +0530

    not completed but

--- EP_08_LetsGetsClassy/src/components/User.js
+++ EP_08_LetsGetsClassy/src/components/User.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 
-const User = () => {
+const User = (props) => {
+    const {name,location}=props
+    const [count,setCount]=useState(0);
+    const incrCount=()=>{
+        setCount(count +1)
+    }
   return (
     <div className='user-card'>     
-      <h2>Name :vikash</h2>
-      <h3>location : patna</h3>
-      <h4>contact : @Viaksh</h4>
+      <h2>Name :{name}</h2>
+      <h3>location : {location}</h3>
+      <h4>contact : @Viaks</h4>
+      <h4>count : {count}</h4>
+      <button onClick={incrCount}>count</button>
     </div>
   )
 }
